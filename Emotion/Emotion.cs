a45b7b2018f2e0ea168b03b961cec043f63f1840
IvayloZhivkov <ivaylovelizarov@gmail.com>
2022-03-26 18:05:30 +0200

    Adding all thing for now

---
+++ Emotion/Emotion.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emotion
+{
+    public class Expression
+    {
+        public void Happy()
+        {
+            Console.WriteLine( "   _________    ");
+            Console.WriteLine(@"  /         \    ");
+            Console.WriteLine(@" /           \   ");
+            Console.WriteLine( "|  |       |  |  ");
+            Console.WriteLine( "|  |       |  |  ");
+            Console.WriteLine( "|             |  ");
+            Console.WriteLine(@" \  \_____/  /   ");
+            Console.WriteLine(@"  \_________/    ");
+
+        }
+        public void Angry()
+        {
+            Console.WriteLine( "   _________      ");
+            Console.WriteLine(@"  /         \    ");
+            Console.WriteLine(@" /   \   /   \   ");
+            Console.WriteLine(@"|    |   |    |  ");
+            Console.WriteLine( "|             |  ");
+            Console.WriteLine( "|    _____    |   ");
+            Console.WriteLine(@" \  /     \  /    ");
+            Console.WriteLine(@"  \_________/     ");
+        }
+
+        public void Sad()
+        {
+            Console.WriteLine("   _________      ");
+            Console.WriteLine(@"  /         \    ");
+            Console.WriteLine(@" /   /   \   \   ");
+            Console.WriteLine(@"|   /|   |\   |  ");
+            Console.WriteLine(@"|             |   ");
+            Console.WriteLine("|    _____    |   ");
+            Console.WriteLine(@" \  /     \  /    ");
+            Console.WriteLine(@"  \_________/     ");
+
+        }
+        public void Sleep()
+        {
+            Console.WriteLine( "   _________      ");
+            Console.WriteLine(@"  /         \   Z ");
+            Console.WriteLine(@" /           \ Z  ");
+            Console.WriteLine(@"|   --   -- Z |   ");
+            Console.WriteLine(@"|         Z   |   ");
+            Console.WriteLine( "|     ---     |   ");
+            Console.WriteLine(@" \           /    ");
+            Console.WriteLine(@"  \_________/     ");
+        }
+        public void Dead()
+        {
+            Console.WriteLine( "   _________     ");
+            Console.WriteLine(@"  /         \    ");
+            Console.WriteLine(@" /  \/   \/  \   ");
+            Console.WriteLine(@"|   /\   /\   |  ");
+            Console.WriteLine(@"|             |  ");
+            Console.WriteLine(@"|      \/     |  ");
+            Console.WriteLine(@" \     /\    /   ");
+            Console.WriteLine(@"  \_________/    ");
+        }
+
+        public void Constipation()
+        {
+            Console.WriteLine(@"   _________   ");
+            Console.WriteLine(@"  /         \  ");
+            Console.WriteLine(@" /  /     \  \ ");
+            Console.WriteLine(@"|   --   --   |");
+            Console.WriteLine(@"|     _  _    |");
+            Console.WriteLine(@"|      \/     |");
+            Console.WriteLine(@" \    _/\_   / ");
+            Console.WriteLine(@"  \_________/  ");
+        }
+        public void Horny()
+        {
+            Console.WriteLine(@"   _________          ");
+            Console.WriteLine(@"  /         \     S   ");
+            Console.WriteLine(@" /           \        ");
+            Console.WriteLine(@"|   ()   --   |       ");
+            Console.WriteLine(@"|             |   E   ");
+            Console.WriteLine(@"|   )-----(   |       ");
+            Console.WriteLine(@" \           /        ");
+            Console.WriteLine(@"  \_________/     X   ");
+        }
+
+        public void Dorcless()
+        {
+            Console.WriteLine("Ó´¬π µ´⁄ ˆ ˜´´∂ ˙´¬π. ˆ åµ ´≈†®´µ´¬¥ ®ˆç˙ å˜∂ ∫´å¨†ˆƒ¨¬.");            
+        }
+    }
+}
---
+++ Emotion/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Emotion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is my emotion?");
+            Console.WriteLine();
+            Console.Write("You are.. ");
+            string command = Console.ReadLine().ToLower();
+            Expression ex = new Expression();
+            while (true)
+            {
+                Console.Clear();
+                switch (command)
+                {   
+                    case "happy":
+                        ex.Happy();
+                        break;
+                    case "angry":
+                        ex.Angry();
+                        break;
+                    case "sad":
+                        ex.Sad();
+                        break;
+                    case "dorcless":
+                        ex.Dorcless();
+                        break;
+                    case "constipated":
+                        ex.Constipation();
+                        break;
+                    case "horny":
+                        ex.Horny();
+                        break;
+                    case "dead":
+                        ex.Dead();
+                        return;                  
+                    case "sleepy":
+                        ex.Sleep();
+                        Console.WriteLine();
+                        Console.WriteLine("Good night");
+                        return;
+                    default:
+                        Console.WriteLine("Unknown emotion!\n Try again.");
+                        break;
+                }
+                Console.WriteLine();
+                Console.WriteLine("What is my emotion?");
+                Console.WriteLine();
+                Console.Write("You are.. ");
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
